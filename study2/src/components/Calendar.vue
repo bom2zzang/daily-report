bb1b0f112c768f9718141e9d5b29bef66cf809b9
bom2zzang <bom2zzang@me.com>
2021-03-21 23:54:04 +0900

    babel-upgrade, vue-full-cal 도전

--- study2/src/components/Calendar.vue
+++ study2/src/components/Calendar.vue
@@ -1,27 +1,61 @@
 <template>
     <div class="calendar">
-        <vue-cal :time="false" active-view="month" :disable-views="['week','day']" :on-event-click="onEventClick" style="height: 500px"/>
+        <!-- <vue-cal :time="false" active-view="month" :disable-views="['week','day']" :on-event-click="onEventClick" style="height: 500px"/> -->
+        <!--test -->
+        <full-calendar :config="config"></full-calendar>
+    
     </div>
 </template>
 
 <script>
-import VueCal from 'vue-cal';
-import 'vue-cal/dist/vuecal.css';
+// import VueCal from 'vue-cal';
+// import 'vue-cal/dist/vuecal.css';
+
+// export default {
+//     name : 'Calendar',
+//     components : {
+//         VueCal
+//     },
+//     methods : {
+//         onEventClick(){
+//             console.log("z");
+//         }
+
+//     },
+//     computed : {},
+//     data(){},
+//     props : {}
+// }
+
+
+import  { FullCalendar }  from 'vue-full-calendar'
+import 'fullcalendar/dist/fullcalendar.css';
+
+console.log(this);
 export default {
     name : 'Calendar',
     components : {
-        VueCal
+        FullCalendar
     },
-    methods : {
-        onEventClick(){
-            console.log("z");
-        }
-
-    },
+    methods : {},
     computed : {},
-    data(){},
+    data(){
+        return {
+            config:{
+                defaultView: 'month',
+                header:{
+                    left:'prev',
+                    center:'title',
+                    right:'next',
+                },
+                height: 500,
+            }
+        }
+    },
     props : {}
 }
+
+
 </script>
 
 <style scoped>
--- study2/src/router/index.js
+++ study2/src/router/index.js
@@ -7,6 +7,8 @@ import About from '@/components/About'
 
 import HelloWorld from '@/components/HelloWorld'
 
+
+
 const routes = [
   {
     path: '/',
